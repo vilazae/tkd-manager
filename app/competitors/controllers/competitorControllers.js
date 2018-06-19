92aece0572d57458de2b2b480930d42b83961aad
vilazae <lazaroezquerro@hotmail.com>
2018-06-19 16:08:17 +0200

    Changing competitor form

--- app/competitors/controllers/competitorControllers.js
+++ app/competitors/controllers/competitorControllers.js
@@ -1,9 +1,9 @@
 tkdApp.controller('competitorController', [ 'apiService', function( apiService ) {
-console.log('ctrl saludo campeonatosController!');	
+console.log('ctrl saludo campeonatosController!');
 	var me = this;
 
 	me.competitorsList = [];
-	
+
 	apiService.sayHello(me.nombre)
 	.then( function (data) {
 		console.log(data.data);
@@ -11,7 +11,7 @@ console.log('ctrl saludo campeonatosController!');
 
  	apiService.getCompetitorsList()
 	.then( function (data) {
-console.log('data -- ',  angular.copy(data));		
+console.log('data -- ',  angular.copy(data));
 		me.competitorsList = data.data;
 me.competitorsList = [{"id":"1","name":"Javier","surname":"Romero","dni":"12345678C","birthday_date":"1995-01-17","gender":"male","liscense":"084-R017","liscense_expiry_date":"2016-06-10","club_id":"1"},{"id":"2","name":"Ainhoa","surname":"Lorza Gamarra","dni":"16111111L","birthday_date":"2001-05-15","gender":"female","liscense":"084-R021","liscense_expiry_date":"2017-05-05","club_id":"1"},{"id":"3","name":"Carlos","surname":"Garc\u00eda","dni":"1666555I","birthday_date":"1992-01-17","gender":"male","liscense":"084-R111","liscense_expiry_date":"2017-01-11","club_id":"2"},{"id":"4","name":"Leyre","surname":"Fern\u00e1ndez","dni":"16666888A","birthday_date":"2000-05-15","gender":"female","liscense":"092-R003","liscense_expiry_date":"2017-01-13","club_id":"3"},
 {"id":"1","name":"Javier","surname":"Romero","dni":"12345678C","birthday_date":"1995-01-17","gender":"male","liscense":"084-R017","liscense_expiry_date":"2016-06-10","club_id":"1"},{"id":"2","name":"Ainhoa","surname":"Lorza Gamarra","dni":"16111111L","birthday_date":"2001-05-15","gender":"female","liscense":"084-R021","liscense_expiry_date":"2017-05-05","club_id":"1"},{"id":"3","name":"Carlos","surname":"Garc\u00eda","dni":"1666555I","birthday_date":"1992-01-17","gender":"male","liscense":"084-R111","liscense_expiry_date":"2017-01-11","club_id":"2"},{"id":"4","name":"Leyre","surname":"Fern\u00e1ndez","dni":"16666888A","birthday_date":"2000-05-15","gender":"female","liscense":"092-R003","liscense_expiry_date":"2017-01-13","club_id":"3"},
@@ -19,7 +19,7 @@ me.competitorsList = [{"id":"1","name":"Javier","surname":"Romero","dni":"123456
 {"id":"1","name":"Javier","surname":"Romero","dni":"12345678C","birthday_date":"1995-01-17","gender":"male","liscense":"084-R017","liscense_expiry_date":"2016-06-10","club_id":"1"},{"id":"2","name":"Ainhoa","surname":"Lorza Gamarra","dni":"16111111L","birthday_date":"2001-05-15","gender":"female","liscense":"084-R021","liscense_expiry_date":"2017-05-05","club_id":"1"},{"id":"3","name":"Carlos","surname":"Garc\u00eda","dni":"1666555I","birthday_date":"1992-01-17","gender":"male","liscense":"084-R111","liscense_expiry_date":"2017-01-11","club_id":"2"},{"id":"4","name":"Leyre","surname":"Fern\u00e1ndez","dni":"16666888A","birthday_date":"2000-05-15","gender":"female","liscense":"092-R003","liscense_expiry_date":"2017-01-13","club_id":"3"},
 {"id":"1","name":"Javier","surname":"Romero","dni":"12345678C","birthday_date":"1995-01-17","gender":"male","liscense":"084-R017","liscense_expiry_date":"2016-06-10","club_id":"1"},{"id":"2","name":"Ainhoa","surname":"Lorza Gamarra","dni":"16111111L","birthday_date":"2001-05-15","gender":"female","liscense":"084-R021","liscense_expiry_date":"2017-05-05","club_id":"1"},{"id":"3","name":"Carlos","surname":"Garc\u00eda","dni":"1666555I","birthday_date":"1992-01-17","gender":"male","liscense":"084-R111","liscense_expiry_date":"2017-01-11","club_id":"2"},{"id":"4","name":"Leyre","surname":"Fern\u00e1ndez","dni":"16666888A","birthday_date":"2000-05-15","gender":"female","liscense":"092-R003","liscense_expiry_date":"2017-01-13","club_id":"3"},
 {"id":"1","name":"Javier","surname":"Romero","dni":"12345678C","birthday_date":"1995-01-17","gender":"male","liscense":"084-R017","liscense_expiry_date":"2016-06-10","club_id":"1"},{"id":"2","name":"Ainhoa","surname":"Lorza Gamarra","dni":"16111111L","birthday_date":"2001-05-15","gender":"female","liscense":"084-R021","liscense_expiry_date":"2017-05-05","club_id":"1"},{"id":"3","name":"Carlos","surname":"Garc\u00eda","dni":"1666555I","birthday_date":"1992-01-17","gender":"male","liscense":"084-R111","liscense_expiry_date":"2017-01-11","club_id":"2"},{"id":"4","name":"Leyre","surname":"Fern\u00e1ndez","dni":"16666888A","birthday_date":"2000-05-15","gender":"female","liscense":"092-R003","liscense_expiry_date":"2017-01-13","club_id":"3"},
-{"id":"1","name":"Javier","surname":"Romero","dni":"12345678C","birthday_date":"1995-01-17","gender":"male","liscense":"084-R017","liscense_expiry_date":"2016-06-10","club_id":"1"},{"id":"2","name":"Ainhoa","surname":"Lorza Gamarra","dni":"16111111L","birthday_date":"2001-05-15","gender":"female","liscense":"084-R021","liscense_expiry_date":"2017-05-05","club_id":"1"},{"id":"3","name":"Carlos","surname":"Garc\u00eda","dni":"1666555I","birthday_date":"1992-01-17","gender":"male","liscense":"084-R111","liscense_expiry_date":"2017-01-11","club_id":"2"},{"id":"4","name":"Leyre","surname":"Fern\u00e1ndez","dni":"16666888A","birthday_date":"2000-05-15","gender":"female","liscense":"092-R003","liscense_expiry_date":"2017-01-13","club_id":"3"}]		
+{"id":"1","name":"Javier","surname":"Romero","dni":"12345678C","birthday_date":"1995-01-17","gender":"male","liscense":"084-R017","liscense_expiry_date":"2016-06-10","club_id":"1"},{"id":"2","name":"Ainhoa","surname":"Lorza Gamarra","dni":"16111111L","birthday_date":"2001-05-15","gender":"female","liscense":"084-R021","liscense_expiry_date":"2017-05-05","club_id":"1"},{"id":"3","name":"Carlos","surname":"Garc\u00eda","dni":"1666555I","birthday_date":"1992-01-17","gender":"male","liscense":"084-R111","liscense_expiry_date":"2017-01-11","club_id":"2"},{"id":"4","name":"Leyre","surname":"Fern\u00e1ndez","dni":"16666888A","birthday_date":"2000-05-15","gender":"female","liscense":"092-R003","liscense_expiry_date":"2017-01-13","club_id":"3"}]
 
 	} );
 
@@ -28,6 +28,7 @@ me.competitorsList = [{"id":"1","name":"Javier","surname":"Romero","dni":"123456
 			tmpComp.selected = false;
 		});
 		competitor.selected = true;
+		competitor.birthday_date = new Date( competitor.birthday_date ).toLocaleDateString();
 		me.selectedCompetitor = angular.copy( competitor );
 		me.showDetail = true;
 
@@ -42,10 +43,10 @@ me.competitorsList = [{"id":"1","name":"Javier","surname":"Romero","dni":"123456
 			me.edit = false;
 		}
 	};
- 
 
-	
-	
-	
-	
+
+
+
+
+
 } ] );
